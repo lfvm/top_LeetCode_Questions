3e26984d7bdb520f69e1171a0c74eb37dc48bca0
lfvm <lfvm200@gmail.com>
2022-11-29 15:14:33 -0600

    added binary search

---
+++ arrays/easy/binary_search.py
@@ -0,0 +1,34 @@
+"""
+704. Binary Search
+
+Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+"""
+
+
+
+class Solution:    
+    def search(self, nums, target: int) -> int:
+        
+        left = 0 
+        right = len(nums) - 1
+        
+        while left <= right:
+            
+            middle = (right + left) // 2
+
+            
+            if target < nums[middle]:
+                right = middle - 1
+            elif target > nums[middle]:
+                left = middle + 1
+            else:
+                return middle 
+            
+        return -1 
+        
+
+ 
+        
